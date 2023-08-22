4f762aaf5db115916ca0ce2b12d75d2e22005118
Maaacs <msb@icomp.ufam.edu.br>
2023-08-21 21:06:02 -0400

    CSS3

--- CSS3/public/script.js
+++ CSS3/public/script.js
@@ -1,11 +1,1 @@
 // Lógica do sistema
-// Escolher qual opção deseja rodar
-(function (){
-    const buttons = document.getElementsByTagName("BUTTON");
-    const cssLogo = document.getElementById("css-logo");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = function(e) {
-            cssLogo.style.position = e.target.innerHTML;
-        };
-    }
-})();
