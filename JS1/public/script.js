3e486e155bbe2b7d5026c7e9ae0a71b38435fee7
Maaacs <msb@icomp.ufam.edu.br>
2023-09-11 20:13:16 -0400

    JS1

--- JS1/public/script.js
+++ JS1/public/script.js
@@ -1,49 +1,8 @@
-// Lógica do sistema
-// Escolher qual opção deseja rodar
-(function (){
-    const video = document.getElementById("video");
-    const play = document.getElementById("play");
-    const pause = document.getElementById("pause");
-    const time = document.getElementById("time");
-    play.onclick = function(){
-        video.play()
-        video.pause()
-        window.setInterval(() =>{
-            time.innerHTML = video.currentTime
-        }, 10);
+for(let i = 1; i < 11; i++){
+    document.write(`<table border=\"1\" class=\"a c\" ${i}>`);
+
+    document.write(`<thead><tr><th colspan=\"2\"> Produtos de ${i} </th></tr></thead>`);
+    for(let j = 1; j < 11; j++){
+        document.write(`<tr><td>${i}X${j}</td><td>${i*j}</td></tr>`);
     }
-})();
-
-// opção 1
-// const botao = document.getElementById("botao");
-// botao.onclick = function (){
-//     console.log("Oi!");
-// }
-
-// opção 2 sem variável global
-// (function (){
-//     const botao = document.getElementById("botao");
-//     let teste = 3;
-//     botao.onclick = function (){
-//     console.log("Oi!");
-//     }
-// })()
-
-
-// incrementando opção 2
-// (function (){
-//     const botao = document.getElementById("botao");
-//     let teste = 3;
-//     botao.onclick = function (){
-//         document.body.style.backgroundColor = "#0000FF";
-//     }
-//     function getRandomColor(){
-//         const letra = "0123456789ABCDEF";
-//         let cor = "#";
-//         for (let i = 0; i < 6; i++){
-//             cor += letra[Math.floor(Math.random()*16)]
-//         }
-//         return cor; 
-//     }
-
-// })()
+}
