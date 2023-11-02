6e32f66bee423bb6d8f3f79ed61b2eb73fc512f6
Maaacs <msb@icomp.ufam.edu.br>
2023-11-01 23:09:39 -0400

    update

--- Game/src/controllers/curso.js
+++ Game/src/controllers/curso.js
@@ -37,14 +37,46 @@ const read = async (req, res) => {
 }
 
 
+// const update = async (req, res) => {
+//     const course = await Curso.findOne({where:{id:req.params.id}});
+//     res.render("curso/update", {
+//         course: course.toJSON()
+//     })
+//  }
+
+
 const update = async (req, res) => {
-    const course = await Curso.findOne({where:{id:req.params.id}});
-    res.render("curso/update", {
-        course: course.toJSON()
-    })
- }
+    if(req.route.methods.get) {
+      const { id } = req.params;
+      const curso = await Curso.findOne({ where: {id}});
+      res.render('curso/update', curso.toJSON());
+    } else {
+      const { id } = req.params
+      const { body } = req;
+      await Curso.update(body, {where: {id}});
+      res.redirect('/curso/read/' + id);
+    }
+  }
 
+  
 
-const remove = async (req, res) => { }
+  const remove = async (req, res) => {
+    if (req.route.methods.delete) {
+      try {
+        const curso = await Curso.findOne({ where: { id: req.params.id } });
+        await curso.destroy();
+        res.redirect("/curso");
+      } catch (err) {
+        throw new Error(err);
+      }
+    } else {
+      const course = await Curso.findOne({ where: { id: req.params.id } });
+      res.render("curso/remove", {
+        course: course.toJSON(),
+      });
+    }
+  };
+  
+
 
 module.exports = {index, create, read, update, remove}
